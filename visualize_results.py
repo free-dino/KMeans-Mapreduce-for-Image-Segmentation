e1d24504d9836952ec9153136e4c1e85b00e991a
sangtran2222004 <sang2222004@gmail.com>
2024-12-05 16:15:20 +0700

    change

--- visualize_results.py
+++ visualize_results.py
@@ -66,12 +66,7 @@ def main(clusters_path, kmeans_img, dst_img):
     kernel = np.ones((5,5), np.uint8)
     img_erosion = cv2.erode(gbr, kernel, iterations=4)
     img_dilation = cv2.dilate(img_erosion, kernel, iterations=3)
-    img = img_dilation.reshape((-1, 3))
-    new_image = np.zeros_like(img)
-    for i in range(img.shape[0]):
-        ind = np.linalg.norm(clusters - img[i], axis=-1).argmin()
-        new_image[i] = clusters[ind].astype(np.uint8)
-    cv2.imwrite(dst_img, new_image.reshape(shape))
+    cv2.imwrite(dst_img,img_dilation)
     print(f'Opening image saved in:{dst_img}')
 
 
